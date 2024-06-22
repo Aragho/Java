cd3d18c4fd71b7618219a131049c114867612015
Aragho <Oluwadanny19@gmail.com>
2024-06-22 16:10:54 +0100

    my assignment

---
+++ RaiseToPower.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+   public class RaiseToPower {
+
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter two number: ");
+    int number1 = input.nextInt();
+    int number2 = input.nextInt();
+
+     for(int index = 1; index; <= number2; index ++){
+			sum*= number1;
+  		
+}
+    System.out.println(" number1 is + the raised to power + number2 is" + sum);
+    }
+   }
