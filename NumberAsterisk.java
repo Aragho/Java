f70f89f785b4078242445ab56f00e7a6747fe401
Aragho <Oluwadanny19@gmail.com>
2024-06-23 00:21:30 +0100

    my assignment

---
+++ NumberAsterisk.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+   public class NumberAsterisk {
+
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+ System.out.print(" Enter number from 1 - 30: ");
+ int number1 = input.nextInt();
+
+ System.out.print(" Enter number from 1 - 30: ");
+ int number2 = input.nextInt();
+
+ System.out.print(" Enter number from 1 - 30: ");
+ int number3 = input.nextInt();
+
+ System.out.print(" Enter number from 1 - 30: ");
+ int number4 = input.nextInt();
+
+ System.out.print(" Enter number from 1 - 30: ");
+ int number5 = input.nextInt();
+
+  
+       for(int index = 1; index <= number1; index++){
+		System.out.print("*");
+	        }
+		System.out.println();
+
+ for(int index = 1; index <= number2; index++){
+		System.out.print("*");
+		}
+		System.out.println();
+
+ for(int index = 1; index <= number3; index++){
+		System.out.print("*");
+	        }
+		System.out.println();
+
+
+ for(int index = 1; index <= number4; index++){
+		System.out.print("*");
+		}
+		System.out.println();
+
+ for(int index = 1; index <= number5; index++){
+		System.out.print("*");
+		}
+		System.out.println();
+
+
+
+}
+}
+
+
+
+
+
+
+
+
