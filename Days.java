b83d1ac8c583e40545739c23e134968fc5e7f009
Aragho <Oluwadanny19@gmail.com>
2024-06-25 22:21:18 +0100

    my assignment

---
+++ Days.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Days{
+
+	public static void main(String[] adenike){
+
+				for(int index = 1; index  <= 12; index++){
+				System.out.println("on the " + index + " day of Christmas,my true love sent to me");
+		
+		switch(index){
+
+     				
+
+				case 12:
+					System.out.println("twelve drummers drumming");
+
+				case 11:
+					System.out.println("eleven eleven pipers piping");
+
+				case 10:
+					System.out.println("ten lords a leaping");
+					
+				case 9:
+					System.out.println("nine ladies dancing");
+				
+				case 8:
+					System.out.println("eight maids a milking");
+				
+				case 7:
+					System.out.println("seven swans a swimming");
+					
+				case 6:
+					System.out.println("six geese a laying");
+					
+				case 5:
+					System.out.println("five gold rings");
+					
+				case 4:
+					System.out.println("four calling birds");
+					
+				case 3: 
+					System.out.println("three french hens");
+					
+				case 2:
+					System.out.println("two people turtledoves");
+					
+				case 1:
+					System.out.println("one partridge in a pear tree");								
+					System.out.println();
+					}
+
+
+
+
+
+
+  			}
+		}
+	}
+
+
+	
+
+
+
+
+	
+
+	
+				
+				
+		
