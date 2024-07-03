cc6c764be090e2dea94035214662a2babfc39247
Aragho <Oluwadanny19@gmail.com>
2024-07-03 18:37:01 +0100

    my assignment

---
+++ isEven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IsEven{
+
+  		 public static void main(String[] args) {
+	    
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter a number: ");
+		 int number = input.nextInt();
+       
+
+		boolean result = IsEvenOrOdd.isEvenOrOdd( number);
+			System.out.println("number is " + result);
+        }
+ }
---
+++ isEvenOrOdd.java
@@ -0,0 +1,20 @@
+
+
+public class IsEvenOrOdd{
+
+  
+	    public static boolean isEvenOrOdd(int number){
+		
+		
+        if (number % 2 == 0) {
+		
+		
+            return true;
+
+        } 
+	else {
+            return false;
+        }
+   }
+
+}
