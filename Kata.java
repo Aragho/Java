b548f26e3855f8183e6f8ad7f68ce707b574cc19
Aragho <Oluwadanny19@gmail.com>
2024-07-03 21:04:54 +0100

    my assignment

---
+++ Kata.java
@@ -0,0 +1,20 @@
+
+
+public class Kata{
+
+  
+	    public static boolean IsEven(int number){
+		
+		
+        if (number % 2 == 0) {
+		
+		
+            return true;
+
+        } 
+	else {
+            return false;
+        }
+   }
+
+}
