3a1500e998d3f2e18be05aa3c88ef0c8825facd9
Aragho <Oluwadanny19@gmail.com>
2024-07-04 13:39:09 +0100

    my assignment

--- Kata.java
+++ Kata.java
@@ -3,18 +3,43 @@
 public class Kata{
 
   
-	    public static boolean IsEven(int number){
+	public static boolean IsEven(int number){
 		
 		
-        if (number % 2 == 0) {
+        	if (number % 2 == 0) {
 		
 		
-            return true;
+            		return true;
 
-        } 
-	else {
-            return false;
-        }
-   }
+        	} 
+		else {
+            		return false;
+        	}
+   	}
+
+
+
+
+
+
+ 
+    public static boolean IsPrimenumber(int number) {
+
+		int numberOfFactor = 0;
+                
+        	for (int index = 2; index <= number; index++) {
+            		if (number % index == 0) numberOfFactor++;
+		
+		}
+		
+		if (numberOfFactor == 1){
+			return true;
+		}
+	
+   		else return false;
+
+   } 
 
 }
+
+                
