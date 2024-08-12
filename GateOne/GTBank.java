235048454c17094b7e85a24aa2505b93dea2b5b4
Aragho <Oluwadanny19@gmail.com>
2024-08-12 04:21:14 +0100

    my code

--- GateOne/GTBank.java
+++ GateOne/GTBank.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java. util.Random;
 
 public class GTBank{
-
+	
 	public static void main(String [] args){
 	Scanner input = new Scanner (System.in);
+	Random random = new Random();
+
+	
+	ArrayList<Integer> accountNumbers = new ArrayList<>();
 
 	ArrayList<String> firstNames = new ArrayList<>();
 
@@ -13,10 +18,11 @@ public class GTBank{
 	ArrayList<Integer> pins = new ArrayList<>();
 
 	ArrayList<Double> balances = new ArrayList<>();
-	
+
+		
 	 boolean exit = false;
 	
-        while (exit) {
+        while (!exit) {
             System.out.println("\nPlease select an option:");
             System.out.println("1. Create Account");
             System.out.println("2. Deposit Money");
@@ -27,31 +33,165 @@ public class GTBank{
             System.out.println("7. Close Account");
             System.out.println("8. Exit");
 
-	int userInput = input.nextInt();
+	int userInput = input.nextInt(); 
+	switch(userInput){
+		case 1: 
+			System.out.println("Create an account");			
+
+			System.out.println("Enter your first name:");
+			String firstName = input.next();
+			firstNames.add(firstName);
+
+			System.out.println("Enter your last name:");
+                   	String lastName = input.next();
+                   	lastNames.add(lastName);
+			
+			int pin;
+			while(true){
+                    	System.out.println("Set your 4-digit PIN:");
+                    	 pin = input.nextInt();
+                    	if(pin >= 1000 && pin <= 9999){
+				break;
+			}else{
+				System.out.println("Invalid PIN. please enter your 4 digit PIN");
+				}
+			}
+			pins.add(pin);
+			
+			
+                    	int accountNumber = random.nextInt(9000000) + 1000000;
+                    	accountNumbers.add(accountNumber);
+                   	balances.add(0.0);
+                       	System.out.println("Account created successfully!");
+			 System.out.println("Your account number is: " + accountNumber);
+                    	break;
+
+		case 2:
+			System.out.println("Enter your account number:");
+			accountNumber = input.nextInt();
+			int index  = accountNumbers.indexOf(accountNumber);
+
+			if (index == -1){
+				System.out.println("Account number not found!");
+				break;
+			}
+		
+			System.out.println("Enter the amount to deposit:");
+			double depositAmount = input.nextDouble();
+
+			double newBalance = balances.get(index) + depositAmount;
+			balances.set(index,newBalance);
+
+			System.out.println("Deposit successful! New Balance:" + newBalance);
+			break;
+
+			
+		case 3:
+			System.out.println("Enter your account number:");
+			 accountNumber = input.nextInt();
+			 index  = accountNumbers.indexOf(accountNumber);
+
+			if (index == -1){
+				System.out.println("Account number not found!");
+				break;
+			}
+
+			
+                    	System.out.println("Enter the amount to withdraw:");
+                    	double withdrawAmount = input.nextDouble();
+	
+			double currentBalance = balances.get(index);
+
+                    if (currentBalance  >= withdrawAmount) {
+                         newBalance = currentBalance - withdrawAmount;
+                        balances.set(index, newBalance);
+                        System.out.println("Withdrawal successful! New balance: " + newBalance);
+                    } else {
+                        System.out.println("Insufficient balance!");
+                    }
+                    break;
+
+		case 4:
+    			System.out.println("Enter your account number:");
+   			accountNumber = input.nextInt();
+    			index = accountNumbers.indexOf(accountNumber);
+
+    			if (index == -1) {
+       			 System.out.println("Account number not found!");
+        		break;
+   			 }
+
+   			 currentBalance = balances.get(index);
+   			 System.out.println("Your balance is: " + currentBalance);
+   			 break;
+
+		case 6:
+			System.out.println("Enter your account number:");
+   			accountNumber = input.nextInt();
+    			index = accountNumbers.indexOf(accountNumber);
+
+    			if (index == -1) {
+       			 System.out.println("Account number not found!");
+        		break;
+   			 }
+			
+			System.out.println("Enter your old PIN:");
+			int oldPin = input.nextInt();
+			
+			if(pins.get(index) != oldPin){
+				System.out.println("Incorrect old PIN");
+			break;
+			}
+			
+			System.out.println("Enter your new PIN:");
+    			int newPin = input.nextInt();
+
+    			while (newPin < 1000 || newPin > 9999) {
+       		 	System.out.println("Invalid PIN. Please enter a 4-digit PIN:");
+        		newPin = input.nextInt();
+   			 }
+
+   			 pins.set(index, newPin);
+   			 System.out.println("PIN changed successfully!");
+   			 break;
+
+		case 7:
+    			System.out.println("Enter your account number:");
+    			accountNumber = input.nextInt();
+    			index = accountNumbers.indexOf(accountNumber);
+
+    			if (index == -1) {
+        		System.out.println("Account number not found!");
+        		break;
+    			}
+
+    // Remove account details from all lists
+    			firstNames.remove(index);
+    			lastNames.remove(index);
+   			 pins.remove(index);
+    			balances.remove(index);
+    			accountNumbers.remove(index);
+
+    			System.out.println("Account closed successfully!");
+    			break;
+			
+		
+
+		case 8:
+			exit = true;
+			break;
+
+		default:
+			System.out.println("Invalid option. please try again!!!!");
+			break;
+			
+
+}
 
 
-	 if (userInput == 1) {
-                createAccount();
-            } else if (userInput == 2) {
-                depositMoney();
-            } else if (userInput == 3) {
-                withdrawMoney();
-            } else if (userInput == 4) {
-                checkBalance();
-            } else if (userInput == 5) {
-                transferMoney();
-            } else if (userInput == 6) {
-                changePin();
-            } else if (userInput == 7) {
-                closeAccount();
-            } else if (userInput == 8) {
-                exit = true;
-            } else {
-                System.out.println("Invalid option. Please try again.");
-            }
-        }
 
-	System.out.println("Thank yoi fir using GTB Bank!");
+
+	System.out.println("Thank you for using GTB Bank!");
 
 
 
@@ -59,9 +199,8 @@ public class GTBank{
 
 		 
 
-
+	}
 
 
 	}
-
 }
